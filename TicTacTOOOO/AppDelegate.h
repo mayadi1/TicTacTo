ec75d46fda41e45c4ae77af691d67306a8391fd2
Jouba Ayedi <joubausa@icloud.com>
2016-05-26 15:25:10 -0500

    Initial Commit

---
+++ TicTacTOOOO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacTOOOO
+//
+//  Created by Mohamed Ayadi on 5/26/16.
+//  Copyright © 2016 Mohamed Ayadi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacTOOOO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacTOOOO
+//
+//  Created by Mohamed Ayadi on 5/26/16.
+//  Copyright © 2016 Mohamed Ayadi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
